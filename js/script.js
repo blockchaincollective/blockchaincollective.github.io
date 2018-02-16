a58140131be6f53d9b51e1ba75f55b07450b2cd9
Richard Chen <richardchen39@gmail.com>
2018-02-16 14:38:21 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function createSlick() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+}
+
+$(document).ready(() => {
+    $('.scrollspy').scrollSpy();
+    $('.parallax').parallax();
+    $('.button-collapse').sideNav();
+});
